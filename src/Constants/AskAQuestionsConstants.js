7d4f73c2a90b145008e4a3350408211b1becf2d2
Arman <11arman@bk.ru>
2024-10-19 03:17:01 -0800

    Rebuilt a more dynamic Delivery

--- src/Constants/AskAQuestionsConstants.js
+++ src/Constants/AskAQuestionsConstants.js
@@ -3,6 +3,86 @@ const ASK_A_QUESTIONS_LIST_STYLES = {
     '&:hover': { textDecoration: 'underline' }
 }
 
+const DELIVERY_TITLE_STYLES = {
+    mb: 6,
+    color: "white",
+    fontSize: { xs: '16px', sm: '18px', md2: '20px' }
+}
+
+const DELIVERY_BOX_STYLES = {
+    display: 'flex',
+    flexDirection: { xs: "column", lg: 'row' }
+}
+
+const FONT_SIZE_FORTY = {
+    fontSize: 40
+}
+
+const FEAUTRE_STYLES = {
+    mb: 2,
+    color: "white",
+    fontSize: { xs: '14px', sm: '16px', md2: '18px' }
+}
+
+const DETAILS_BUTTON_STYLES = {
+    fontSize: { xs: '11px', sm: '16px', md2: '18px' }
+}
+
+const DELIVERY_CARTON_STYLES = {
+    display: { xs: 'none', sm2: 'block' }
+}
+
+const DELIVERY_BOX_STYLES_SECOND = {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    minHeight: 600,
+    borderRadius: 8,
+    background: "white",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+    p: 2,
+}
+
+const DELIVERY_BOX_STYLES_THIRD = {
+    display: "flex",
+    justifyContent: "space-between",
+    height: 'auto'
+}
+
+const DELIVERY_BOX_STYLES_FORTH = {
+    display: "flex",
+    flexWrap: 'wrap', gap: { xs: '61px', md2: '18px' },
+    width: { xs: '100%', md2: '80%' },
+    height: 'auto',
+    justifyContent: { xs: 'center', sm: 'space-between' }
+}
+
+const DELIVERY_BOX_STYLES_FIFTH = {
+    width: 500,
+    height: 500,
+    display: { xs: 'none', md2: 'flex' },
+    width: '100%',
+    alignItems: "center",
+    justifyContent: "flex-end",
+}
+
+const DELIVERY_IMAGE_STYLES = {
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
+    borderRadius: 14
+}
 export {
     ASK_A_QUESTIONS_LIST_STYLES,
+    DELIVERY_TITLE_STYLES,
+    DELIVERY_BOX_STYLES,
+    FONT_SIZE_FORTY,
+    FEAUTRE_STYLES,
+    DETAILS_BUTTON_STYLES,
+    DELIVERY_CARTON_STYLES,
+    DELIVERY_BOX_STYLES_SECOND,
+    DELIVERY_BOX_STYLES_THIRD,
+    DELIVERY_BOX_STYLES_FORTH,
+    DELIVERY_BOX_STYLES_FIFTH,
+    DELIVERY_IMAGE_STYLES
 }
--- src/Routes/AskAQuestion/Navigation/Delivery.jsx
+++ src/Routes/AskAQuestion/Navigation/Delivery.jsx
@@ -4,11 +4,11 @@ import CancelScheduleSendIcon from "@mui/icons-material/CancelScheduleSend";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import { useContext, useRef } from "react";
 import ThemeModes from "../../../Components/Shared/ThemeModes";
-import ToggleButtons from "../../../Components/Orders/RightSide/ToggleButtons";
 import { LangContext } from "../../../Contexts/LangContext";
 import BoxMui from "../../../Components/Shared/BoxMui";
-import './Responsive.css';
+import './responsive.css';
 import './delivery.css';
+import { DELIVERY_BOX_STYLES, DELIVERY_BOX_STYLES_FIFTH, DELIVERY_BOX_STYLES_FORTH, DELIVERY_BOX_STYLES_SECOND, DELIVERY_BOX_STYLES_THIRD, DELIVERY_CARTON_STYLES, DELIVERY_IMAGE_STYLES, DELIVERY_TITLE_STYLES, DETAILS_BUTTON_STYLES, FEAUTRE_STYLES, FONT_SIZE_FORTY } from "../../../Constants/AskAQuestionsConstants";
 
 const Address = ({ address, text }) => (
   <Box sx={{ display: "flex", flexDirection: "column", mt: 3, mb: 1, gap: 1 }}>
@@ -35,6 +35,12 @@ const Delivery = () => {
     />
   ));
 
+  const sectionContent = [
+    { tag: "h3", text: `${prefix}.lots of`, sx: { mb: 2 } },
+    { tag: "p", text: `${prefix}.pick-up points`, sx: { mb: 5 } },
+    { tag: "h3", text: `${prefix}.info`, sx: { mb: 2 } }
+  ];
+
   return (
     <ThemeModes tagName='simpleDiv' id="delivery">
       <ThemeModes tagName="h3" style={{ marginBottom: "22px" }}>
@@ -42,66 +48,43 @@ const Delivery = () => {
       </ThemeModes>
       <Box className="box">
         <Box className="box-content">
-          <Typography variant="h4" sx={{ mb: 6, color: "white", fontSize: { xs: '16px', sm: '18px', md2: '20px' } }}>
+          <Typography variant="h4" sx={DELIVERY_TITLE_STYLES}>
             {t(`${prefix}.box title`)}
           </Typography>
-          <Box sx={{ display: 'flex', flexDirection: { xs: "column", lg: 'row' } }}>
+          <Box sx={DELIVERY_BOX_STYLES}>
             {["feature1", "feature2", "feature3"].map((feature, index) => (
               <span className="feature" key={feature}>
-                {index === 0 && <LocalShippingIcon sx={{ fontSize: 40 }} />}
-                {index === 1 && <AutorenewIcon sx={{ fontSize: 40 }} />}
-                {index === 2 && <CancelScheduleSendIcon sx={{ fontSize: 40 }} />}
-                <Typography variant="h5" sx={{ mb: 2, color: "white", fontSize: { xs: '14px', sm: '16px', md2: '18px' } }}>
-                  {t(`${prefix}.${feature}`)}
-                </Typography>
+                {index === 0 && <LocalShippingIcon sx={FONT_SIZE_FORTY} />}
+                {index === 1 && <AutorenewIcon sx={FONT_SIZE_FORTY} />}
+                {index === 2 && <CancelScheduleSendIcon sx={FONT_SIZE_FORTY} />}
+                <Typography variant="h5" sx={FEAUTRE_STYLES}> {t(`${prefix}.${feature}`)} </Typography>
               </span>
             ))}
           </Box>
           <Button className="details" onClick={scrollToBox}>
-            <Typography variant="h6" sx={{ fontSize: { xs: '11px', sm: '16px', md2: '18px' } }}>{t(`${prefix}.details`)}</Typography>
+            <Typography variant="h6" sx={DETAILS_BUTTON_STYLES}>{t(`${prefix}.details`)}</Typography>
           </Button>
         </Box>
-        <Box className="image-container" sx={{ display: { xs: 'none', sm2: 'block' } }}>
+        <Box className="image-container" sx={DELIVERY_CARTON_STYLES}>
           <img src="/deliveryBox1.png" alt="Carton Boxes" />
         </Box>
       </Box>
-      <ThemeModes tagName="h3" sx={{ mb: 2 }}>
-        {t(`${prefix}.lots of`)}
-      </ThemeModes>
-      <ThemeModes tagName="p" sx={{ mb: 5 }}>
-        {t(`${prefix}.pick-up points`)}
-      </ThemeModes>
-      <ThemeModes tagName="h3" sx={{ mb: 2 }}>
-        {t(`${prefix}.info`)}
-      </ThemeModes>
-      <BoxMui
-        ref={targetBoxRef}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          minHeight: 600,
-          borderRadius: 8,
-          background: "white",
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-          p: 2,
-        }}
-      >
-        <Box sx={{ display: "flex", justifyContent: "space-between", height: 'auto' }}>
-          <Box sx={{ display: "flex", flexWrap: 'wrap', gap: { xs: '61px', md2: '18px' }, width: { xs: '100%', md2: '80%' }, height: 'auto', justifyContent: { xs: 'center', sm: 'space-between' } }}>
 
+      {sectionContent.map((item, index) => (
+        <ThemeModes key={index} tagName={item.tag} sx={item.sx}> {t(item.text)} </ThemeModes>
+      ))}
+
+      <BoxMui ref={targetBoxRef} sx={DELIVERY_BOX_STYLES_SECOND} >
+        <Box sx={DELIVERY_BOX_STYLES_THIRD}>
+          <Box sx={DELIVERY_BOX_STYLES_FORTH}>
             {renderAddresses()}
           </Box>
-          <Box sx={{ width: 500, height: 500, display: { xs: 'none', md2: 'flex' }, width: '100%', alignItems: "center", justifyContent: "flex-end", }}>
-            <img
-              src="/cardboard-box.webp"
-              alt="Carton Boxes"
-              style={{ width: "100%", height: "100%", objectFit: "contain", borderRadius: 14 }}
-            />
+          <Box sx={DELIVERY_BOX_STYLES_FIFTH}>
+            <img src="/cardboard-box.webp" alt="Carton Boxes" style={DELIVERY_IMAGE_STYLES} />
           </Box>
         </Box>
       </BoxMui>
-    </ThemeModes >
+    </ThemeModes>
   );
 };
 
